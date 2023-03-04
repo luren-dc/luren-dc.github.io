ec6c9fe8383f1e444aa647acffd234a57d3e7f8c
Luren <3308862290@qq.com>
2023-03-04 23:20:12 +0800

    fix: 修复Bug

--- source/js/copyright.js
+++ source/js/copyright.js
@@ -1,3 +1,5 @@
-document.body.addEventListener("copy", () => {
+document.body.addEventListener("copy", (e) => {
+  let clipboardData = e.clipboardData || window.clipboardData;
+  if (!clipboardData) return;
   btf.snackbarShow("复制成功 请遵守 CC BY-NC-SA 4.0 协议");
 });
